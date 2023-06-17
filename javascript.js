a654725f1553624b068a23e41b8ab3317dcfd195
Mike Salcedo <mikesalcedo191@gmail.com>
2023-06-16 23:43:07 -0400

    Updating my calculator project working on step 6

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let numberA = []; // Variables for the calculator
+let numberB;
+let operatorSelected;
+
 function add(numberOne, numberTwo) {
   return parseInt(numberOne) + parseInt(numberTwo);
 }
@@ -10,10 +14,6 @@ function multiply(numberOne, numberTwo) {
 function divide(numberOne, numberTwo) {
   return parseInt(numberOne) / parseInt(numberTwo);
 }
-
-let numberA; // Variables for the calculator
-let numberB;
-let operatorSelected;
 
 // Operate function takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
@@ -40,24 +40,73 @@ function operate(operator, numberOne, numberTwo) {
 
 // Targeting the buttons for the calculator
 
-const buttons = document.querySelectorAll(".calculatorBtn");
+const numberButtons = document.querySelectorAll(".calculatorBtn");
 
 // we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
+numberButtons.forEach((button) => {
   // and for each one we add a 'click' listener
   button.addEventListener("click", (e) => {
-    console.log(e.target.id); // Get ID of Clicked Element
+    // console.log(e.target.id); // Get ID of Clicked Element
     updateCalculatorScreen(e);
   });
 });
 
 //Functions that populate the display when you click the number buttons
 
-// Target numbersoperated p tags
+// Target numbersOperated p tags
 
-const numbersoperated = document.querySelector(".numbersOperated");
-numbersoperated.textContent = `test`;
+let numberAOutput;
+
+const numbersOperated = document.querySelector(".numbersOperated");
 
 function updateCalculatorScreen(e) {
-  numbersoperated.textContent = `${e.target.id}`;
+  tempNumber.push(e.target.id);
+  numbersOperated.textContent = `${tempNumber.join("")}`;
+
+  if (numberOne === undefined) {
+    numberA = tempNumber.join("");
+    console.log(numberA);
+
+    return numberA;
+  } else {
+    numberB = tempNumber.slice(numberA[-1], -1);
+    console.log(numberB);
+
+    return numberB;
+  }
 }
+
+// Get numbers to run operations on
+
+let tempNumber = [];
+
+function getNumbers(e) {
+  if (e.target.classList.contains("division")) {
+    numberOne = numberA.slice(0, -1);
+  } else if (e.target.classList.contains("multiplication")) {
+    numberOne = numberA.slice(0, -1);
+  } else if (e.target.classList.contains("minus")) {
+    numberOne = numberA.slice(0, -1);
+  } else if (e.target.classList.contains("equals")) {
+    numberOne = numberA.slice(0, -1);
+  } else if (e.target.classList.contains("plus")) {
+    numberOne = numberA.slice(0, -1);
+  }
+}
+
+// numberA = numberAOutput.split();
+
+// Make the calculator work! You’ll need to store the first number that is input into the calculator when a user presses an operator, and also save which operation has been chosen and then operate() on them when the user presses the “=” key.
+
+const operatorButton = document.querySelectorAll(".operation");
+
+// function operate(numberOne = numberAOutput) {}
+
+operatorButton.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    getNumbers(e);
+  });
+});
+
+let numberOne;
+let numberTwo;
